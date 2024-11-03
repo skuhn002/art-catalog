a16b843e9c6485d2b9474d5924eec691a80e25f9
Sam Kuhn <kuhnsamuel2@gmail.com>
2024-11-03 06:00:25 -0600

    EXP1-0005: Add Flow to LWC - 1.00

--- Dad's Art Catalog/force-app/main/default/lwc/webContainer/webContainer.js
+++ Dad's Art Catalog/force-app/main/default/lwc/webContainer/webContainer.js
@@ -1,3 +1,17 @@
 import { LightningElement } from 'lwc';
+// import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 
-export default class WebContainer extends LightningElement {}
+
+export default class WebContainer extends LightningElement {
+    // handleCompletion(event) {
+    //     if (event.detail.status === 'FINISHED') {
+    //         // Handle flow completion
+    //         const toastEvent = new ShowToastEvent({
+    //             title: 'Flow Finished',
+    //             message: 'The flow has completed successfully.',
+    //             variant: 'success',
+    //         });
+    //         this.dispatchEvent(toastEvent);
+    //     }
+    // }
+}
