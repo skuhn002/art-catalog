27dbf51a0a6e9313e591a0d147dbe442ea39cf76
Sam Kuhn <kuhnsamuel2@gmail.com>
2024-11-05 17:04:33 -0600

    Art Catalog Items can be Edited - 1.00

--- Dad's Art Catalog/force-app/main/default/lwc/selectedArt/selectedArt.js
+++ Dad's Art Catalog/force-app/main/default/lwc/selectedArt/selectedArt.js
@@ -4,17 +4,13 @@ export default class SelectedArt extends LightningElement {
     @api
     selectedRecord;
 
-    connectedCallback() {
-        // console.log('TEST 1');
-        // console.log('(Child) selectedRecord: ');
-        // console.log(this.selectedRecord.Title__c);
+    editMode = false;
+
+    toggleEditMode(){
+        this.editMode = !this.editMode;
     }
 
-    // selectedRecord.Title__c
-    // selectedRecord.Imugr_Link__c
-    // selectedRecord.Artist__c
-    // selectedRecord.Purchase_Price__c
-    // selectedRecord.Estimated_Value__c
-    // selectedRecord.Date_Acquired__c
-    // selectedRecord.Location__c
+    handleSuccess(){
+        this.editMode = false;
+    }
 }
