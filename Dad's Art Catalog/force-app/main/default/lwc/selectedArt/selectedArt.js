6d6248a818e87ddc750dcf6d91d2611a58e31423
Sam Kuhn <kuhnsamuel2@gmail.com>
2024-11-07 16:27:28 -0600

    Add Additional Costs Flow Works :) - 1.00

--- Dad's Art Catalog/force-app/main/default/lwc/selectedArt/selectedArt.js
+++ Dad's Art Catalog/force-app/main/default/lwc/selectedArt/selectedArt.js
@@ -1,10 +1,56 @@
 import { api, LightningElement, track } from 'lwc';
+import { FlowAttributeChangeEvent } from 'lightning/flowSupport';
 
 export default class SelectedArt extends LightningElement {
-    @api
-    selectedRecord;
-
+    @track showFlow = false;
+    @track _selectedRecord;
+    
     editMode = false;
+
+    @track inputVariables;
+
+    @api
+    set selectedRecord(record) {
+        this._selectedRecord = record;
+        // Optionally, you can start the flow here if needed
+        this.updateInputVariables();
+        if (this._selectedRecord && this._selectedRecord.Id) {
+            console.log('About to fire notifyFlowOfChange');
+            this.notifyFlowOfChange();
+        }
+    }
+
+    get selectedRecord() {
+        return this._selectedRecord;
+    }
+
+    notifyFlowOfChange() {
+        console.log('Inside notifyFlowOfChange');
+        console.log('this.selectedRecord.Id: ' + this.selectedRecord.Id);
+        const attributeChangeEvent = new FlowAttributeChangeEvent('Related_Art_Id', this.selectedRecord.Id);
+        this.dispatchEvent(attributeChangeEvent);
+    }
+
+    updateInputVariables() {
+        if (this.selectedRecord) {
+            this.inputVariables = [{
+                name: "Related_Art_Id",
+                type: "String",
+                value: this.selectedRecord.Id
+            }];
+            this.toggleFlowVisibility();
+        } else {
+            this.inputVariables = [];
+        }
+    }
+
+    toggleFlowVisibility() {
+        // This will cause the flow to re-render with new variables
+        this.showFlow = false;
+        setTimeout(() => {
+            this.showFlow = true;
+        }, 100); // A small delay to ensure the DOM updates
+    }
 
     toggleEditMode(){
         this.editMode = !this.editMode;
@@ -22,7 +68,6 @@ export default class SelectedArt extends LightningElement {
     }
 
     get imgurLiveLink(){
-        console.log('Inside getImgurLiveLink');
         let theId = this.selectedRecord.Imugr_Link__c.split('_d')[0].split('/').at(-1);
         let theLink = 'https://imgur.com/' + theId;
 
--- Dad's Art Catalog/force-app/main/default/lwc/webContainer/webContainer.js
+++ Dad's Art Catalog/force-app/main/default/lwc/webContainer/webContainer.js
@@ -8,13 +8,20 @@ export default class WebContainer extends LightningElement {
     @track
     artRecordsMap = new Map();
 
-    @track
-    selectedRecord;
+    @track _selectedRecord;
+
+    set selectedRecord(record){
+        this._selectedRecord = record;
+    };
+
+    get selectedRecord(){
+        return this._selectedRecord;
+    }
 
     connectedCallback() {
         getArt().then(result => {
-            console.log('result: ');
-            console.log(result);
+            // console.log('result: ');
+            // console.log(result);
             this.artRecords = result;
 
             result.forEach((record, index) => {
@@ -26,17 +33,17 @@ export default class WebContainer extends LightningElement {
     selectArt(event) {
         const itemDiv = event.currentTarget.attributes;
         const index = Number(event.currentTarget.getAttribute('data-index'));
-        console.log(this.artRecordsMap.get(index));
+        // console.log(this.artRecordsMap.get(index));
         this.selectedRecord = this.artRecordsMap.get(index);
     }
 
     handleArtUpdate(event){
-        console.log('event.detail: ');
-        console.log(event.detail);
+        // console.log('event.detail: ');
+        // console.log(event.detail);
 
         getArt().then(result => {
-            console.log('result: ');
-            console.log(result);
+            // console.log('result: ');
+            // console.log(result);
             this.artRecords = result;
 
             result.forEach((record, index) => {
